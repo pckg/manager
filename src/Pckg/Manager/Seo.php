ca5dda12eefcb97c8668797e4d2243b7028f1983
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-10-05 12:05:39 +0200

    Implemented title, description, keywords and image

--- src/Pckg/Manager/Seo.php
+++ src/Pckg/Manager/Seo.php
@@ -3,9 +3,81 @@
 class Seo
 {
 
+    protected $templates = [
+        'title'       => '<title>##</title>',
+        'description' => '<meta name="description" content="##" />',
+        'keywords'    => '<meta name="keywords" content="##" />',
+    ];
+
+    protected $title;
+
+    protected $description;
+
+    protected $keywords;
+
+    protected $image;
+
     public function __toString()
     {
-        return '';
+        return (
+               $this->title
+                   ? str_replace('##', trim(strip_tags($this->title)), $this->templates['title'])
+                   : ''
+               ) . (
+               $this->description
+                   ? str_replace('##', trim(strip_tags($this->description)), $this->templates['description'])
+                   : ''
+               ) . (
+               $this->keywords
+                   ? str_replace('##', trim(strip_tags($this->keywords)), $this->templates['keywords'])
+                   : ''
+               ) . $this->getOgTags();
+    }
+
+    public function getOgTags()
+    {
+        $image = $this->image
+            ? htmlspecialchars(config('url') . str_replace(' ', '%20', $this->image))
+            : '';
+        $title = trim(strip_tags($this->title));
+        $description = trim(strip_tags($this->description));
+
+        return '<meta property="og:title" content="' . $title . '" />
+		<meta property="og:site_name" content="' . $title . '" />
+		<meta property="og:description" content="' . $description . '" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="' . router()->getUri(false) . '" />
+		<meta property="fb:admins" content="1197210626" />
+		<meta property="fb:app_id" content="' . config('pckg.manager.seo.app_id') . '" />
+		<meta property="og:image" content="' . $image . '" />';
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = trim(strip_tags($description));
+
+        return $this;
+    }
+
+    public function setKeywords($keywords)
+    {
+        $this->keywords = $keywords;
+
+        return $this;
+    }
+
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
     }
 
 }
