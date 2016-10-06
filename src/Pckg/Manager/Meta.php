3a079812dfe980dc34a82b335eac76cc32aa6393
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-10-06 17:29:06 +0200

    Add sumoMe and tawkTo

--- src/Pckg/Manager/Meta.php
+++ src/Pckg/Manager/Meta.php
@@ -9,7 +9,7 @@ class Meta
     {
         $this->add(
             [
-                'name'    => 'viewport',
+                'name' => 'viewport',
                 'content' => 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
             ]
         );
@@ -38,6 +38,10 @@ class Meta
 
     public function addGoogleAnalytics($trackingId)
     {
+        if (!$trackingId) {
+            return;
+        }
+
         $this->add(
             '<script>
   (function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){
@@ -50,6 +54,36 @@ class Meta
 
 </script>'
         );
+    }
+
+    public function addTawkTo($id)
+    {
+        if (!$id) {
+            return;
+        }
+
+        $this->add(
+            '<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src=\'https://embed.tawk.to/' . $id . '/default\';
+s1.charset=\'UTF-8\';
+s1.setAttribute(\'crossorigin\',\'*\');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>'
+        );
+    }
+
+    public function addSumoMe($id)
+    {
+        if (!$id) {
+            return;
+        }
+
+        $this->add('<script src="//load.sumome.com/" data-sumo-site-id="' . $id . '" async="async"></script>');
     }
 
     public function __toString()
