8728ed40c27903bea12c97f32fef555b9852ca23
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-10-16 21:06:27 +0200

    Implement googleRemarketingTag

--- src/Pckg/Manager/Meta.php
+++ src/Pckg/Manager/Meta.php
@@ -159,6 +159,30 @@ s0.parentNode.insertBefore(s1,s0);
         );
     }
 
+    public function addGoogleRemarketingTag($id) {
+        if (!$id) {
+            return;
+        }
+
+        $this->add('<!-- Google Code for Remarketing Tag -->
+<script type="text/javascript">
+    /* <![CDATA[ */
+    var google_conversion_id = ' . $id . ';
+    var google_custom_params = window.google_tag_params;
+    var google_remarketing_only = true;
+    /* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+    <div style="display:inline;">
+        <img height="1" width="1" style="border-style:none;" alt=""
+             src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/' . $id . '/?value=0&amp;guid=ON&amp;script=0"/>
+    </div>
+</noscript>
+', 'footer');
+    }
+
     public function getMeta($onlySections = [])
     {
         if (!is_array($onlySections)) {
