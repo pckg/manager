d2b9476b44f6f04e9c8201b692d3e3e7aae4a633
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-12-01 15:53:03 +0100

    Breadcrumbs

--- src/Pckg/Manager/Page.php
+++ src/Pckg/Manager/Page.php
@@ -8,4 +8,30 @@ class Page
         return in_array(router()->getUri(), ['/', '/dev.php', '/dev.php/']);
     }
 
+    public function getBreadcrumbs()
+    {
+        $breadcrumbs = [];
+
+        /**
+         * Dashboard is always displayed.
+         */
+        $breadcrumbs['/maestro'] = __('breadcrumbs.dashboard');
+
+        /**
+         * Check for table listing.
+         */
+        if ($table = router()->resolved('table')) {
+            $breadcrumbs['/dynamic/tables/list/' . $table->id] = $table->title;
+        }
+
+        /**
+         * Add current page.
+         */
+        if (!array_key_exists(router()->getUri(), $breadcrumbs)) {
+            $breadcrumbs[router()->getUri()] = __('breadcrumbs.current');
+        }
+
+        return $breadcrumbs;
+    }
+
 }
