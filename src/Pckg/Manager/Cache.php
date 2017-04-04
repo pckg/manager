d1a3184d81241edda374c7d7be65eeb26b972191
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-04-04 14:00:02 +0200

    Cache

---
+++ src/Pckg/Manager/Cache.php
@@ -0,0 +1,61 @@
+<?php namespace Pckg\Manager;
+
+class Cache
+{
+
+    protected $handlers = [];
+
+    public function __construct()
+    {
+        foreach (config('pckg.cache.handler') as $type => $config) {
+            $this->registerHandler($type, $config);
+        }
+    }
+
+    protected function registerHandler($type, $config)
+    {
+        $handler = (new $config['handler']);
+        $handler->setNamespace($type . '*');
+
+        return $this->handlers[$type] = $handler;
+    }
+
+    /**
+     * @return \Doctrine\Common\Cache\Cache
+     */
+    public function getAppCache()
+    {
+        return $this->handlers['app'];
+    }
+
+    /**
+     * @return \Doctrine\Common\Cache\Cache
+     */
+    public function getRequestCache()
+    {
+        return $this->handlers['request'];
+    }
+
+    /**
+     * @return \Doctrine\Common\Cache\Cache
+     */
+    public function getSessionCache()
+    {
+        return $this->handlers['session'];
+    }
+
+    public function cache($key, $val, $type = 'request', $time = 0)
+    {
+        $cache = $this->handlers[$type];
+
+        if (!$cache->contains($key)) {
+            $value = $val();
+            $cache->save($key, $value);
+
+            return $value;
+        }
+
+        return $cache->fetch($key);
+    }
+
+}
