b868831f7885c235cd6436105ab719155e06cfd5
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-04-05 00:44:00 +0200

    Google fonts middleware

---
+++ src/Pckg/Manager/Middleware/RegisterGoogleFonts.php
@@ -0,0 +1,25 @@
+<?php namespace Pckg\Manager\Middleware;
+
+use Pckg\Manager\Asset;
+
+class RegisterGoogleFonts
+{
+
+    /**
+     * @var Asset
+     */
+    protected $asset;
+
+    public function __construct(Asset $asset)
+    {
+        $this->asset = $asset;
+    }
+
+    public function execute()
+    {
+        foreach (config('pckg.manager.asset.googleFonts', []) as $font) {
+            $this->asset->addGoogleFont($font['font'], $font['weight'] ?? '400,700', $font['set'] ?? 'latin,latin-ext');
+        }
+    }
+
+}
