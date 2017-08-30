a7f0d753345cc936a5056ebf7521af59218d2420
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-08-30 15:33:26 +0200

    Dont show image when not set

--- src/Pckg/Manager/Seo.php
+++ src/Pckg/Manager/Seo.php
@@ -71,8 +71,8 @@ class Seo
 		<meta property="og:type" content="website" />
 		<meta property="og:url" content="' . router()->getUri(false) . '" />
 		<meta property="fb:admins" content="1197210626" />
-		<meta property="fb:app_id" content="' . config('pckg.manager.seo.app_id') . '" />
-		<meta property="og:image" content="' . $image . '" />';
+		<meta property="fb:app_id" content="' . config('pckg.auth.provider.facebook.config.app_id') . '" />
+		' . ($image ? '<meta property="og:image" content="' . $image . '" />' : '');
     }
 
     public function setTitle($title)
