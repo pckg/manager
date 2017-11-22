74df55adfc495ea1ce6ce9c5d652d1e815b8d356
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-11-22 18:20:10 +0100

    Locale

---
+++ src/Pckg/Manager/Locale/Locale.php
@@ -0,0 +1,24 @@
+<?php namespace Pckg\Manager\Locale;
+
+use NumberFormatter;
+
+class Locale
+{
+
+    protected $locale;
+
+    public function __construct($locale)
+    {
+        $this->locale = $locale;
+    }
+
+    public function decimal($number)
+    {
+        $formatter = new NumberFormatter($this->locale, NumberFormatter::DECIMAL);
+        $formatter->setAttribute(\NumberFormatter::MIN_FRACTION_DIGITS, 0);
+        $formatter->setAttribute(\NumberFormatter::MAX_FRACTION_DIGITS, 100);
+
+        return $formatter->format($number);
+    }
+
+}
