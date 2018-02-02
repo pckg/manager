cb4af0a8de3651fbe4b4f18967ffacdbb1966cae
Bojan Rajh <schtr4jh@schtr4jh.net>
2018-02-02 23:18:58 +0100

    Do not load font iw wasnt set

--- src/Pckg/Manager/Middleware/RegisterGoogleFonts.php
+++ src/Pckg/Manager/Middleware/RegisterGoogleFonts.php
@@ -20,6 +20,13 @@ class RegisterGoogleFonts
         foreach (config('pckg.manager.asset.googleFonts', []) as $font) {
             $this->asset->addGoogleFont($font['font'], $font['weight'] ?? '400,700', $font['set'] ?? 'latin,latin-ext');
         }
+        foreach ([config('design.vars.fontFamilySFirst', 'design.vars.fontFamilyPFirst')] as $font) {
+            if (!$font) {
+                continue;
+            }
+
+            $this->asset->addGoogleFont($font, '400,700', 'latin,latin-ext');
+        }
 
         return $next();
     }
