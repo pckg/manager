00cc2f713f7df8182cf1ae810c029b2669ff4744
Bojan Rajh <schtr4jh@schtr4jh.net>
2018-05-12 13:16:50 +0200

    Cleanup filters

--- src/Pckg/Manager/Asset/PathPckgFilter.php
+++ src/Pckg/Manager/Asset/PathPckgFilter.php
@@ -1,93 +1,17 @@
 <?php namespace Pckg\Manager\Asset;
 
 use Assetic\Asset\AssetInterface;
-use Assetic\Factory\AssetFactory;
 use Assetic\Filter\BaseNodeFilter;
 use Assetic\Filter\DependencyExtractorInterface;
+use Assetic\Filter\LessFilter;
 use Assetic\Util\LessUtils;
 
-class PathPckgFilter extends BaseNodeFilter implements DependencyExtractorInterface
+class PathPckgFilter extends LessFilter
 {
-
-    private $nodeBin;
-
-    /**
-     * @var array
-     */
-    private $treeOptions;
-
-    /**
-     * @var array
-     */
-    private $parserOptions;
-
-    /**
-     * Load Paths
-     *
-     * A list of paths which less will search for includes.
-     *
-     * @var array
-     */
-    protected $loadPaths = [];
-
-    /**
-     * Constructor.
-     *
-     * @param string $nodeBin   The path to the node binary
-     * @param array  $nodePaths An array of node paths
-     */
-    public function __construct($nodeBin = '/usr/bin/node', array $nodePaths = [])
-    {
-        $this->nodeBin = $nodeBin;
-        $this->setNodePaths($nodePaths);
-        $this->treeOptions = [];
-        $this->parserOptions = [];
-    }
-
-    /**
-     * @param bool $compress
-     */
-    public function setCompress($compress)
-    {
-        $this->addTreeOption('compress', $compress);
-    }
-
-    public function setLoadPaths(array $loadPaths)
-    {
-        $this->loadPaths = $loadPaths;
-    }
-
-    /**
-     * Adds a path where less will search for includes
-     *
-     * @param string $path Load path (absolute)
-     */
-    public function addLoadPath($path)
-    {
-        $this->loadPaths[] = $path;
-    }
-
-    /**
-     * @param string $code
-     * @param string $value
-     */
-    public function addTreeOption($code, $value)
-    {
-        $this->treeOptions[$code] = $value;
-    }
-
-    /**
-     * @param string $code
-     * @param string $value
-     */
-    public function addParserOption($code, $value)
-    {
-        $this->parserOptions[$code] = $value;
-    }
 
     public function filterLoad(AssetInterface $asset)
     {
-        $source = $asset->getSourceDirectory() . path('ds') . $asset->getSourcePath();
+        $source = $asset->getSourceRoot() . path('ds') . $asset->getSourcePath();
         $sourceHash = sha1($source . filemtime($source));
         $input = path('tmp') . $sourceHash . '.pathfilter.tmp';
 
@@ -97,7 +21,7 @@ class PathPckgFilter extends BaseNodeFilter implements DependencyExtractorInterf
             /**
              * Fix ../ and similar urls.
              */
-            $sourceDir = $asset->getSourceDirectory();
+            $sourceDir = $asset->getSourceRoot();
             $rootDir = path('root');
 
             /**
@@ -122,9 +46,13 @@ class PathPckgFilter extends BaseNodeFilter implements DependencyExtractorInterf
                          * We have to move $dots times towards root.
                          */
                         //$resourcePathResolved = realpath($sourceDir . path('ds') . str_repeat('..' . path('ds'), $dots));
-                        $resourcePathUnresolved = implode(path('ds'), array_slice(explode(path('ds'), $sourceDir), 0,
-                                                                                  substr_count($sourceDir, path('ds')) -
-                                                                                  $dots + 1));
+                        $resourcePathUnresolved = implode(
+                            path('ds'), array_slice(
+                                          explode(path('ds'), $sourceDir), 0,
+                                          substr_count($sourceDir, path('ds')) -
+                                          $dots + 1
+                                      )
+                        );
                         $relativeDir = str_replace($rootDir, path('ds'), $resourcePathUnresolved) . path('ds');
 
                         $urlPart = strpos($value, 'url("') !== false
@@ -165,49 +93,5 @@ class PathPckgFilter extends BaseNodeFilter implements DependencyExtractorInterf
 
     public function filterDump(AssetInterface $asset)
     {
-    }
-
-    /**
-     * @todo support for import-once
-     * @todo support for import (less) "lib.css"
-     */
-    public function getChildren(AssetFactory $factory, $content, $loadPath = null)
-    {
-        $loadPaths = $this->loadPaths;
-        if (null !== $loadPath) {
-            $loadPaths[] = $loadPath;
-        }
-
-        if (empty($loadPaths)) {
-            return [];
-        }
-
-        $children = [];
-        foreach (LessUtils::extractImports($content) as $reference) {
-            if ('.css' === substr($reference, -4)) {
-                // skip normal css imports
-                // todo: skip imports with media queries
-                continue;
-            }
-
-            if ('.less' !== substr($reference, -5)) {
-                $reference .= '.less';
-            }
-
-            foreach ($loadPaths as $loadPath) {
-                if (file_exists($file = $loadPath . '/' . $reference)) {
-                    $coll = $factory->createAsset($file, [], ['root' => $loadPath]);
-                    foreach ($coll as $leaf) {
-                        $leaf->ensureFilter($this);
-                        $children[] = $leaf;
-                        goto next_reference;
-                    }
-                }
-            }
-
-            next_reference:
-        }
-
-        return $children;
     }
 }
