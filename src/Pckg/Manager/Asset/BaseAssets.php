b0f44cfab55f6864d8416e216914ee1abaefeb83
Bojan Rajh <schtr4jh@schtr4jh.net>
2019-03-13 12:47:13 +0100

    Vendor components

--- src/Pckg/Manager/Asset/BaseAssets.php
+++ src/Pckg/Manager/Asset/BaseAssets.php
@@ -46,7 +46,7 @@ trait BaseAssets
     {
         $this->addAssets(
             [
-                "components/jquery/jquery.min.js",
+                "vendor/components/jquery/jquery.min.js",
                 'vendor/pckg/framework/src/Pckg/Framework/public/js/serializeObject.jquery.js',
             ],
             'libraries'
