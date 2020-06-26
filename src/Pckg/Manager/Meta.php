b5266c60daf25026240b787ae9b997b81cf80f7a
Bojan Rajh <schtr4jh@schtr4jh.net>
2020-06-26 20:51:14 +0200

    Remove version from fb chat

--- src/Pckg/Manager/Meta.php
+++ src/Pckg/Manager/Meta.php
@@ -333,7 +333,7 @@ s0.parentNode.insertBefore(s1,s0);
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = \'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1\';
+  js.src = \'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js\';
   fjs.parentNode.insertBefore(js, fjs);
 }(document, \'script\', \'facebook-jssdk\'));', 'body.first');
 
