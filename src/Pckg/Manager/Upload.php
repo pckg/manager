1b0b8c2d2bc71a480c28dfc1e72a5a1d00db49e6
Bojan Rajh <schtr4jh@schtr4jh.net>
2020-07-14 19:18:59 +0200

    Update file uploads

--- src/Pckg/Manager/Upload.php
+++ src/Pckg/Manager/Upload.php
@@ -57,30 +57,83 @@ class Upload
         return true;
     }
 
+    /**
+     * @param $file
+     * @return false|string
+     */
+    private function getExtension($file)
+    {
+        return substr($file, strrpos($file, '.'));
+    }
+
     public function save($dir, $name = null)
     {
         $file = $this->getFile();
 
         if (!$file) {
-            return false;
+            throw new \Exception('No file');
         }
 
+        /**
+         * When name is not fixed, convert it to safe name.
+         */
+        $fixedName = !!$name;
         if (!$name) {
             $name = Convention::url(substr($file['name'], 0, strrpos($file['name'], '.')));
+        } else if (strpos($name, '..') !== false) {
+            throw new \Exception('Invalid filename');
+        } else if ($this->getExtension($file['name']) !== $this->getExtension($name)) {
+            throw new \Exception('Invalid extension');
         }
-        $extension = substr($file['name'], strrpos($file['name'], '.'));
-        $i = 0;
-        do {
-            $filename = $name . ($i ? '_' . $i : '') . $extension;
-            $i++;
-        } while (is_file($dir . $filename));
 
+        /**
+         * Check that file does not exist.
+         */
+        if (!$fixedName) {
+            $extension = $this->getExtension($file['name']);
+            $i = 0;
+            do {
+                $filename = $name . ($i ? '_' . $i : '') . $extension;
+                $i++;
+            } while (is_file($dir . $filename));
+        } else {
+            $filename = $name;
+        }
+
+        /**
+         * Make sure that uploads dir exists.
+         */
         if (!is_dir($dir)) {
             mkdir($dir, 0777, true);
         }
 
-        move_uploaded_file($file['tmp_name'], $dir . $filename);
+        /**
+         * Make sure that custom dir exists.
+         */
+        if (strpos($filename, '/') !== false) {
+            $exploded = explode('/', $filename);
+            $dirs = implode('/', array_slice($exploded, 0, -1));
+            $realdir = $dir . $dirs;
+            if (!is_dir($realdir)) {
+                mkdir($realdir, 0777, true);
+            }
+        }
 
+        /**
+         * Save file to final dir.
+         */
+        if (!file_exists($dir . $filename)) {
+            $okay = move_uploaded_file($file['tmp_name'], $dir . $filename);
+            if (!$okay) {
+                throw new \Exception('Error moving uploaded file');
+            }
+        } elseif (false && sha1(file_get_contents($dir . $filename)) !== sha1(file_get_contents($file['tmp_name']))) {
+            throw new \Exception('Delete existing file first');
+        }
+
+        /**
+         * Set and return uploaded filename.
+         */
         return $this->uploadedFilename = $filename;
     }
 
