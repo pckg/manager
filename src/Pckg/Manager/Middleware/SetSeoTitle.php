c27655b1662189ee6944700e0aab06b1eaa82366
Bojan Rajh <schtr4jh@schtr4jh.net>
2020-12-12 17:34:49 +0100

    Seo title middleware

---
+++ src/Pckg/Manager/Middleware/SetSeoTitle.php
@@ -0,0 +1,17 @@
+<?php namespace Pckg\Manager\Middleware;
+
+class SetSeoTitle
+{
+
+    public function execute(callable $next)
+    {
+        /**
+         * Set title if not set yet.
+         */
+        $seoTitle = router()->get('tags')['seo:title'] ?? null;
+        seoManager()->setTitle(($seoTitle ? $seoTitle . ' - ' : null) . config('site.title'));
+
+        return $next();
+    }
+
+}
