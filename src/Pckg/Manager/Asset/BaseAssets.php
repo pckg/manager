908c5a45e566996b34c83abb0eae4e674400a57a
Bojan Rajh <schtr4jh@schtr4jh.net>
2021-02-05 22:22:05 +0100

    Remove sortable

--- src/Pckg/Manager/Asset/BaseAssets.php
+++ src/Pckg/Manager/Asset/BaseAssets.php
@@ -52,7 +52,7 @@ trait BaseAssets
                 "node_modules/vue/dist/vue.min.js",
                 //'node_modules/vuex/dist/vuex.min.js',
                 // 'node_modules/vue-router/dist/vue-router.min.js',
-                'node_modules/sortablejs/dist/sortable.umd.js',
+                //'node_modules/sortablejs/dist/sortable.umd.js',
                 'node_modules/vuedraggable/dist/vuedraggable.umd.min.js',
             ],
             'libraries'
